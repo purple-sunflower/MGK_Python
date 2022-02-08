2215694d3c4e22836043821cb1ddd54a578b1fe1
Purple-sunflower <78474087+purple-sunflower@users.noreply.github.com>
2022-02-08 10:57:53 +0900

    Add files via upload

---
+++ day1.py
@@ -0,0 +1,2 @@
+print("Hello World!/n")
+print("오늘은 파이썬 1일차!")
---
+++ day3.py
@@ -0,0 +1,16 @@
+print("1") #1번 문제
+
+my_name = "보라해바라기"
+my_age = 23
+my_height = 164.5
+
+print(type(my_age))
+print(type(my_height))
+print(type(10>11))
+print(type(my_name))
+
+print("2") #2번 문제
+
+introduce = "제 이름은 보라해바라기입니다."
+print(introduce[6:12]) 
+print(introduce[-10:-4]) #(-)를 이용해서
