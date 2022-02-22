dac2fd936f7b3f6997e60ffadec57261f233bf75
Purple-sunflower <78474087+purple-sunflower@users.noreply.github.com>
2022-02-22 11:06:35 +0900

    Add files via upload

---
+++ day12.py
@@ -0,0 +1,12 @@
+from re import A
+
+
+exam = ("Hello", "This is Python", "Bye")
+
+examOne = len(exam[0])
+examTwo = len(exam[1])
+examThree = len(exam[2])
+
+exam_dict = {"Hello" : examOne , "This is Python" : examTwo, "Bye" : examThree}
+
+print(exam_dict)
